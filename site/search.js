e8d782ac584a919503e75017cd2abd4a30f15dcc
Breck Yunits <breck@Brecks-MacBook-Pro.local>
2022-09-18 19:29:38 -1000

    checkpoint

---
+++ site/search.js
@@ -0,0 +1,60 @@
+let searchIndex = false
+
+const initSearchAutocomplete = elementId => {
+  const input = document.getElementById(elementId)
+  const urlParams = new URLSearchParams(window.location.search)
+  const query = urlParams.get("q")
+  if (query) input.value = query
+  autocomplete({
+    input,
+    minLength: 1,
+    emptyMsg: "No matching entities found",
+    preventSubmit: true,
+    fetch: async (query, update) => {
+      text = query.toLowerCase()
+      // you can also use AJAX requests instead of preloaded data
+
+      // if (!searchIndex) {
+      //   let response = await fetch("/searchIndex.json")
+      //   if (response.ok) searchIndex = await response.json()
+      // }
+
+      searchIndex = []
+
+      const suggestions = searchIndex.filter(entity =>
+        entity.label.toLowerCase().startsWith(text)
+      )
+
+      suggestions.push({
+        label: `Full text search for "${query.replace(/</g, "&lt;")}"`,
+        appeared: "",
+        id: ""
+      })
+
+      update(suggestions)
+    },
+    onSelect: item => {
+      const isLocalHost =
+        location.hostname === "localhost" || location.hostname === "127.0.0.1"
+
+      const { id } = item
+      const goToUrl = `/treatments/${id}.html`
+      if (id)
+        window.location = isLocalHost
+          ? goToUrl
+          : `https://cancerdb.com${goToUrl}`
+      else {
+        const goToUrl = `/search?q=${encodeURIComponent(
+          document.getElementById(elementId).value
+        )}`
+        window.location = isLocalHost
+          ? goToUrl
+          : `https://heal.cancerdb.com${goToUrl}`
+      }
+    }
+  })
+}
+
+document.addEventListener("DOMContentLoaded", evt =>
+  initSearchAutocomplete("headerSearch")
+)
