76d599faa76b55f7c424b1ea9cacc5e34a0053ad
Breck Yunits <breck@Brecks-MacBook-Pro.local>
2022-12-03 06:43:09 -1000

    Update JTree and switch to TreeBase SearchServer

--- code/HealServer.ts
+++ code/HealServer.ts
@@ -12,6 +12,7 @@ const numeral = require("numeral")
 const { Disk } = require("jtree/products/Disk.node.js")
 const { TreeNode } = jtree
 const { TreeBaseFolder } = require("jtree/products/treeBase.node.js")
+const { SearchServer } = require("jtree/products/treeBaseSearchServer.node.js")
 const { ScrollFile, getFullyExpandedFile } = require("scroll-cli")
 
 const baseFolder = path.join(__dirname, "..")
@@ -19,21 +20,17 @@ const databaseFolder = path.join(baseFolder, "database")
 const ignoreFolder = path.join(baseFolder, "ignore")
 const builtSiteFolder = path.join(baseFolder, "site")
 
-const base = new TreeBaseFolder()
+const folder = new TreeBaseFolder()
 	.setDir(path.join(databaseFolder, "things"))
 	.setGrammarDir(path.join(databaseFolder, "grammar"))
 	.loadFolder()
-
-const searchLogPath = path.join(ignoreFolder, "searchLog.tree")
-Disk.touch(searchLogPath)
 
 const scrollSettings = getFullyExpandedFile(
 	path.join(builtSiteFolder, "settings.scroll")
 ).code
 
 class HealServer {
-	homepage = this.scrollToHtml(`
- `)
+	homepage = Disk.read(path.join(builtSiteFolder, "index.html"))
 
 	app = undefined
 	constructor() {
@@ -60,92 +57,28 @@ class HealServer {
 		app.use(express.static(__dirname))
 		app.use(express.static(builtSiteFolder))
 
-		const searchCache = {}
+		const searchServer = new SearchServer(folder)
+		const searchLogPath = path.join(ignoreFolder, "searchLog.tree")
+		Disk.touch(searchLogPath)
 
+		const searchHTMLCache = {}
 		app.get("/search", (req, res) => {
 			const originalQuery = req.query.q ?? ""
-			const tree = `search
- time ${Date.now()}
- ip ${req.ip}
- query
-  ${originalQuery.replace(/\n/g, "\n  ")} 
-`
-			fs.appendFile(searchLogPath, tree, function() {})
 
-			if (searchCache[originalQuery]) {
-				res.send(searchCache[originalQuery])
-				return
-			}
-			searchCache[originalQuery] = this.scrollToHtml(
-				this.search(decodeURIComponent(originalQuery))
-			)
+			searchServer.logQuery(searchLogPath, originalQuery, req.ip)
 
-			res.send(searchCache[originalQuery])
+			if (!searchHTMLCache[originalQuery])
+				searchHTMLCache[originalQuery] = this.scrollToHtml(
+					searchServer.search(
+						originalQuery,
+						"html",
+						["id", "title", "type", "appeared"],
+						"id"
+					)
+				)
+
+			res.send(searchHTMLCache[originalQuery])
 		})
-	}
-
-	search(query): string {
-		const startTime = Date.now()
-		// Todo: allow advanced search. case sensitive/insensitive, regex, et cetera.
-		const testFn = str => str?.includes(query)
-
-		const escapedQuery = htmlEscaped(query)
-		const hits = base.filter(file => testFn(file.toString()))
-		const nameHits = [] // base.filter(file => file.lowercaseNames.some(testFn))
-		const baseUrl = "https://cancerdb.com/treatments/"
-
-		const highlightHit = file => {
-			const line = file
-				.toString()
-				.split("\n")
-				.find(line => testFn(line))
-			return line.replace(query, `<span style="highlightHit">${query}</span>`)
-		}
-		const fullTextSearchResults = hits
-			.map(
-				file =>
-					` <div class="searchResultFullText"><a href="${baseUrl}${
-						file.permalink
-					}">${file.title}</a> - ${file.get("type")} #${
-						file.rank
-					} - ${highlightHit(file)}</div>`
-			)
-			.join("\n")
-
-		const nameResults = nameHits
-			.map(
-				file =>
-					` <div class="searchResultName"><a href="${baseUrl}${
-						file.permalink
-					}">${file.title}</a> - ${file.get("type")} #${file.rank}</div>`
-			)
-			.join("\n")
-
-		const time = numeral((Date.now() - startTime) / 1000).format("0.00")
-		return `
-html
- <div class="pldbSearchForm"><form style="display:inline;" method="get" action="https://heal.cancerdb.com/search"><input name="q" placeholder="Search" autocomplete="off" type="search" id="searchFormInput"><input class="pldbSearchButton" type="submit" value="Search"></form></div>
- <script>document.addEventListener("DOMContentLoaded", evt => initSearchAutocomplete("searchFormInput"))</script>
-
-* <p class="searchResultsHeader">Searched ${numeral(0).format(
-			"0,0"
-		)} treatments and entities for "${escapedQuery}" in ${time}s.</p><hr>
-
-html
- <p class="searchResultsHeader">Showing ${
-		nameHits.length
- } files whose name or aliases matched.</p>
-
-html
-${nameResults}
-<hr>
-
-* <p class="searchResultsHeader">Showing ${
-			hits.length
-		} files who matched on a full text search.</p>
-
-html
- ${fullTextSearchResults}`
 	}
 
 	listen(port = 4444) {
