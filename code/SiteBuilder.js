87ddeeb56538f64485bcb0ab069c7a18fc9ee615
Breck Yunits <breck@Brecks-MacBook-Pro.local>
2022-12-10 13:38:41 -1000

    Remove typescript

---
+++ code/SiteBuilder.js
@@ -0,0 +1,67 @@
+#!/usr/bin/env node
+const path = require("path")
+const { Disk } = require("jtree/products/Disk.node.js")
+const { TreeBaseFolder } = require("jtree/products/treeBase.node.js")
+
+const baseFolder = path.join(__dirname, "..")
+const databaseFolder = path.join(baseFolder, "database")
+const builtSiteFolder = path.join(baseFolder, "site")
+
+const folder = new TreeBaseFolder()
+  .setDir(path.join(databaseFolder, "things"))
+  .setGrammarDir(path.join(databaseFolder, "grammar"))
+  .loadFolder()
+
+class TreeBasePageTemplate {
+  toScroll() {
+    const { file, typeName } = this
+    const { title, id } = file
+
+    return `title ${title}
+
+import settings.scroll
+htmlTitle ${title} - ${lodash.upperFirst(typeName)}
+
+html
+ <a class="prevLang" href="${this.prevPage}">&lt;</a>
+ <a class="nextLang" href="${this.nextPage}">&gt;</a>
+
+viewSourceUrl https://github.com/breck7/CancerDB/blob/main/database/things/${id}.cancerdb
+
+html
+ <div class="quickLinks">${this.quickLinks}</div>
+
+keyboardNav ${this.prevPage} ${this.nextPage}
+`.replace(/\n\n\n+/g, "\n\n")
+  }
+
+  get typeName() {
+    return ""
+  }
+
+  get quickLinks() {
+    return ""
+  }
+
+  get prevPage() {
+    return ""
+  }
+
+  get nextPage() {
+    return ""
+  }
+}
+
+class SiteBuilder {
+  buildDatabasePagesCommand() {
+    folder.forEach(file => {
+      Disk.write(
+        path.join(builtSiteFolder, `${file.id}.scroll`),
+        new TreeBasePageTemplate(file).toScroll()
+      )
+    })
+  }
+}
+
+if (!module.parent)
+  runCommand(new SiteBuilder(), process.argv[2], process.argv[3])
---
+++ code/test.js
@@ -0,0 +1,42 @@
+#!/usr/bin/env node
+
+const tap = require("tap")
+const { ScrollFolder } = require("scroll-cli")
+
+const runTree = testTree =>
+  Object.keys(testTree).forEach(key => {
+    testTree[key](tap.equal)
+  })
+
+const testTree = {}
+
+testTree.ensureNoErrorsInScrollExtensions = areEqual => {
+  const scrollFolder = new ScrollFolder(__dirname)
+  const { grammarErrors } = scrollFolder
+  if (grammarErrors.length) console.log(grammarErrors)
+  areEqual(grammarErrors.length, 0, "no errors in scroll extensions")
+}
+
+// todo
+testTree.ensureNoErrorsInBlog = areEqual => {
+  const checkScroll = folderPath => {
+    const folder = new ScrollFolder(folderPath)
+    areEqual(
+      folder.grammarErrors.length,
+      0,
+      `no grammarErrors in ${folderPath}`
+    )
+    //areEqual(folder.errors.length, 0, `no errors in ${folderPath}`)
+  }
+}
+
+testTree.ensureFieldsAreTrimmed = areEqual => {
+  const scrollFolder = new ScrollFolder(__dirname)
+  const { grammarErrors } = scrollFolder
+  if (grammarErrors.length) console.log(grammarErrors)
+  areEqual(grammarErrors.length, 0, "no errors in scroll extensions")
+}
+
+if (module && !module.parent) runTree(testTree)
+
+module.exports = { testTree }
