6c46875115883c882de9b5f91d7a17774174a888
Breck Yunits <breck@Brecks-MacBook-Pro.local>
2022-12-12 11:46:58 -1000

    command updates

--- code/build.js
+++ code/build.js
@@ -1,8 +1,4 @@
 #!/usr/bin/env node
 const { TreeBaseBuilder } = require("jtree/products/treeBase.node.js")
-const { ScrollFolder } = require("scroll-cli")
-
 const { folder, builtSiteFolder } = require("./folder.js")
-
 new TreeBaseBuilder(folder).compileTreeBaseFilesToScrollFiles(builtSiteFolder)
-new ScrollFolder(builtSiteFolder).buildFiles()
