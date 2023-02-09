e3a77a1cf3578353a08fc5e95a03f0801e6b9181
breck7 <breck7@gmail.com>
2023-02-09 15:45:36 +0000

    Deploying to github.cancerdb.com from @ breck7/CancerDB@be728876a0e0ea0e9a879aa41e2f0296bd59e0a1 🚀

--- search.js
+++ search.js
@@ -1,5 +1,29 @@
 let searchIndex = false
+let searchIndexRequestMade = false
 
+const SearchSuggestionInterface = {
+  label: "string",
+  appeared: "number",
+  id: "string",
+  url: "string"
+}
+
+const tinyTypeScript = (items, expectedInterface) =>
+  items.forEach(item => {
+    Object.keys(item).forEach(key => {
+      const value = item[key]
+      const actualType = typeof item[key]
+      const expectedType = expectedInterface[key]
+      const passed = actualType === expectedType
+      console.assert(passed)
+      if (!passed)
+        console.error(
+          `For key '${key}' object had type '${actualType}' but expected '${expectedType}'. Value was '${value}'`
+        )
+    })
+  })
+
+// This method is currently used to enable autocomplete on: the header search, front page search, 404 page search
 const initSearchAutocomplete = elementId => {
   const input = document.getElementById(elementId)
   const urlParams = new URLSearchParams(window.location.search)
@@ -14,43 +38,33 @@ const initSearchAutocomplete = elementId => {
       text = query.toLowerCase()
       // you can also use AJAX requests instead of preloaded data
 
-      // if (!searchIndex) {
-      //   let response = await fetch("/searchIndex.json")
-      //   if (response.ok) searchIndex = await response.json()
-      // }
-
-      searchIndex = []
+      if (!searchIndexRequestMade) {
+        searchIndexRequestMade = true
+        let response = await fetch("/searchIndex.json")
+        if (response.ok) searchIndex = await response.json()
+      }
 
       const suggestions = searchIndex.filter(entity =>
         entity.label.toLowerCase().startsWith(text)
       )
 
+      const htmlEncodedQuery = query.replace(/</g, "&lt;")
+
       suggestions.push({
-        label: `Full text search for "${query.replace(/</g, "&lt;")}"`,
-        appeared: "",
-        id: ""
+        label: `Full text search for "${htmlEncodedQuery}"`,
+        appeared: 2022,
+        id: "",
+        url: `/fullTextSearch?q=${htmlEncodedQuery}`
       })
+      tinyTypeScript(suggestions, SearchSuggestionInterface)
 
       update(suggestions)
     },
     onSelect: item => {
-      const isLocalHost =
-        location.hostname === "localhost" || location.hostname === "127.0.0.1"
-
-      const { id } = item
-      const goToUrl = `/treatments/${id}.html`
-      if (id)
-        window.location = isLocalHost
-          ? goToUrl
-          : `https://cancerdb.com${goToUrl}`
-      else {
-        const goToUrl = `/search?q=${encodeURIComponent(
-          document.getElementById(elementId).value
-        )}`
-        window.location = isLocalHost
-          ? goToUrl
-          : `https://heal.cancerdb.com${goToUrl}`
-      }
+      const { url, id } = item
+      if (id) window.location = url
+      else
+        window.location = "/fullTextSearch?q=" + encodeURIComponent(input.value)
     }
   })
 }
